a491f8ff80934fcbe249f3160f6b1b225ec7a0f5
Tafita Rakotozandry <tafitarakoto@Tafitas-MacBook-Pro.local>
2022-04-07 09:58:52 -0400

    Commented out the main for a later testing

--- CuringCamPAINS/Controller/main.cpp
+++ CuringCamPAINS/Controller/main.cpp
@@ -10,38 +10,36 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     //    QCoreApplication a(argc, argv); //Need to initialize
+
+    ////Used to test the database class
     //        Database db("../../database.sqlite");
-//    string parameters[] = {"userId", "name", "password", "userName","isResearcher"};
-//    string values[] = {"4", "Lita", "pluss", "litat","0"};
+    //    string parameters[] = {"userId", "name", "password", "userName","isResearcher"};
+    //    string values[] = {"4", "Lita", "pluss", "litat","0"};
     //  db.write("user",parameters,5,values);
     //    db.remove("user","name","Lita");
-//    Login l("../../database.sqlite");
-//    l.signUp("hounasm","look");
 
-    //Saved Messages Testing:
-//    SavedMessages s("../../database.sqlite");
-//    s.createMessage("Test","testing this message");
-//    s.deleteMessage("Test");
+    ////Used to test the login class
+    //        Login l("../../database.sqlite");
+    //        l.signUp("hounasm","look");
 
 
-//    //    QCoreApplication a(argc, argv); //Need to initialize
-//    //        Database db("../../database.sqlite");
-//    string parameters[] = {"userId", "name", "password", "userName","isResearcher"};
-//    string values[] = {"4", "Lita", "pluss", "litat","0"};
-//    //  db.write("user",parameters,5,values);
-//    //    db.remove("user","name","Lita");
-//    Login login("../../database.sqlite");
-//    login.signUp("hounasm","look");
+    ////Used to test the saved messages
+    //    SavedMessages s("../../database.sqlite");
+    //    s.createMessage("Test","testing this message");
+    //    s.deleteMessage("Test");
 
-//    ContactList cl;
-//    cl.getUserInput();
 
-    //Custom Testing:
-Custom cust;
-//cust.displayGuide();
-//cust.addBackground();
-//cust.levelUp();
-//cust.getLevel();
-cust.updateNotification();
+    ////Used to test the contact List
+    //    ContactList cl;
+    //    cl.getUserInput();
+
+
+    //Used to test the Custom
+    Custom cust;
+    //cust.displayGuide();
+    //cust.addBackground();
+    //cust.levelUp();
+    //cust.getLevel();
+    cust.updateNotification();
     return 0;
 }
