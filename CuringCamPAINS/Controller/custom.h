1f8b2a1ed8540b4f1d83c88f8914ae0da8b34efd
Rory Schauder <roryschauder@Rorys-MacBook-Pro.local>
2022-04-06 22:35:20 -0400

    updates

--- CuringCamPAINS/Controller/custom.h
+++ CuringCamPAINS/Controller/custom.h
@@ -2,17 +2,34 @@
 #define CUSTOM_H
 #include <iostream>
 #include <fstream>
+#include <time.h>
+#include <stdlib.h>
 
 class Custom
 {
 public:
     Custom();
 
+    /**
+     * @brief Function increments the level of the user.
+     */
     void levelUp();
-    void addBackground();
-    void updateNotification();
-    void displayGuide();
+    /**
+     * @brief Function simply returns the private level variable of the Custom class.
+     * @return integer current level of the user
+     */
     int getLevel();
+
+    void addBackground();
+    /**
+     * @brief updateNotification outputs an encouraging message to the user on every Sunday or Monday.
+     */
+    void updateNotification();
+    /**
+     * @brief Function displays a User Guide to the user upon request.
+     */
+    void displayGuide();
+
 private:
     int level = 1;
 };
