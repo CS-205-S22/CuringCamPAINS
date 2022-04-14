78657fc7194f23204860489b80e31d3ee8858232
Tafita Rakotozandry <tafitarakoto@Tafitas-MacBook-Pro.local>
2022-04-14 09:06:57 -0400

    Updated the login page connection

--- CuringCamPAINS/gui/logingui.cpp
+++ CuringCamPAINS/gui/logingui.cpp
@@ -23,22 +23,11 @@ void LoginGUI::on_submission_pressed()
     string password=pwd.toStdString();
 
     s = new Login("../../../../../database.sqlite");
-    //        s.signUp("hounasm","look");
-    //        s.close();
-
     //    if(username ==  "test" && password == "test") {
     if(s->signUp(username,password)==true) {
         //        QMessageBox::information(this, "Login", "Username and password is correct");
-//        hide();
-//        dashBoard = new Dashboard(this);
-//        cout<<"Here2"<<endl;
-//        dashBoard->show();
-
-        QMessageBox::information(this, "Login", "Username and password is correct");
-
-        //        dashBoard=new Dashboard(this);
-        //        this->hide();
-        //        dashBoard->show();
+        dashBoard = new Dashboard(this);
+        dashBoard->show();
 
     }
     else {
--- CuringCamPAINS/gui/registrationgui.cpp
+++ CuringCamPAINS/gui/registrationgui.cpp
@@ -15,17 +15,17 @@ registrationGUI::~registrationGUI()
 
 void registrationGUI::on_buttonBox_accepted()
 {
-    Database db("../../../../../database.sqlite");
+    dbb=new Database("../../../../../database.sqlite");
     string new_name=(ui->name->text()).toStdString();
     string new_username=(ui->username->text()).toStdString();
     string new_password=(ui->password->text()).toStdString();
     string new_isResearcher=(ui->isResearcher->text()).toStdString();
 
-    string new_id= std::to_string((db.getMaxId("user","userId"))+1);
+    string new_id= std::to_string((dbb->getMaxId("user","userId"))+1);
 
     string parameters[]={"userId","name","password","userName","isResearcher"};
     string values[]={new_id, new_name,new_password,new_username,new_isResearcher};
-    db.write("user",parameters,5,values);
+    dbb->write("user",parameters,5,values);
 
 }
 
