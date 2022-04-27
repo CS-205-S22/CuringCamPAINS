135f66b605434ece1898e238438de8553e43fe8d
Joshua Garcia <joshuagarcia@Joshuas-MacBook-Air.local>
2022-04-27 09:41:21 -0400

    added error handeling and fixed delete unitTest

--- CuringCamPAINS/gui/loggui.cpp
+++ CuringCamPAINS/gui/loggui.cpp
@@ -25,7 +25,20 @@ void LogGui::autofill() {
     int mon = 1 + ltm->tm_mon;
     int day = ltm->tm_mday;
     int year = 1900 + ltm->tm_year;
-    string date = to_string(mon) + "/" + to_string(day) + "/" + to_string(year);
+
+    string day_string = to_string(day);
+    string mon_string = to_string(mon);
+    string year_string = to_string(year);
+
+    if(day < 10){
+        day_string = "0" + day_string;
+    }
+    if(mon < 10){
+        mon_string = "0" + mon_string;
+    }
+
+
+    string date = mon_string + "/" + day_string + "/" + year_string;
 
     ui->lineEdit_name->setText(name);
     ui->lineEdit_committed->setText(committed);
@@ -51,6 +64,15 @@ void LogGui::on_pushButton_enter_clicked()
         QMessageBox::information(this,tr("INVALID AGE"), tr("This person has to be 18 or older!"));
         //what else can I do because if I type 'a' then that will still be accepted as a name
 
+    //CHECK IF THE PHONE-NUMBER IS VALID :)
+    }else if (ui->lineEdit_phoneNumber->text().toStdString().size() != 10){
+        QMessageBox::information(this,tr("INVALID PHONE-NUMBER"), tr("The format for the phone-number is \n XXXXXXXXXX"));
+
+
+    //CHECK IF THE DATE IS CORRECT
+    }else if (ui->lineEdit_date->text().toStdString().size() != 10){
+        QMessageBox::information(this,tr("INVALID DATE"), tr("The format for the date is \n mm/dd/yyyy"));
+
     //CHECK IF VALID isCommitted RESPONSE
     }else if(ui->lineEdit_committed->text().toStdString() != "False" &&
              ui->lineEdit_committed->text().toStdString() != "false" &&
@@ -70,22 +92,8 @@ void LogGui::on_pushButton_enter_clicked()
     logForm.date = ui->lineEdit_date->text().toStdString();
     logForm.committed = ui->lineEdit_committed->text().toStdString();
 
-    /*logForm.logInput[0] = ui->nameLineEdit->text().toStdString();
-    logForm.logInput[1] = ui->ageLineEdit->text().toStdString();
-    logForm.logInput[2] = ui->pnLineEdit->text().toStdString();
-    logForm.logInput[3] = ui->numAttemptsLineEdit->text().toStdString();
-    logForm.logInput[4] = ui->methodOfContactLineEdit->text().toStdString();
-    logForm.logInput[5] = ui->reactionLineEdit->text().toStdString();
-    logForm.logInput[6] = ui->dateContactedLineEdit->text().toStdString();
-    logForm.logInput[7] = ui->isCommittedLineEdit->text().toStdString();
-
-    logForm.saveLogForm(logForm.logInput[0],logForm.logInput[1],logForm.logInput[2],
-            logForm.logInput[3],logForm.logInput[4],logForm.logInput[5],
-            logForm.logInput[6],logForm.logInput[7]);*/
-
     logForm.saveLogForm(logForm.name, logForm.age, logForm.phoneNumber, logForm.numOfAttempts,
                        logForm.methodOfContact, logForm.reaction, logForm.date, logForm.committed);
-    //logForm.saveLogForm("John Cena", "19", "1111112", "3", "text", "unsure", "April 7th, 2022", "false");
     //logForm.deleteLog("111");
     }
 }
